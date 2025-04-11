c585488b072bdd9fdb77a7dfd503e9d63f60dcec
FutureTerminal <zyanw236@gmail.com>
2025-04-11 14:46:59 -0400

    switching to postgres

--- Redllow/settings.py
+++ Redllow/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Redllow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'redllow_db',
+        'USER': 'redllow_user',
+        'PASSWORD': 'Dengming1972!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- page/admin.py
+++ page/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Review
+from .models import Region, Prices, Neighbourhood, ZipCode, Review
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('user', 'region', 'rating', 'created_at')
@@ -7,5 +7,9 @@ class ReviewAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'region__region_name', 'content')
 
 admin.site.register(Review, ReviewAdmin)
+admin.site.register(Region)
+admin.site.register(Prices)
+admin.site.register(Neighbourhood)
+admin.site.register(ZipCode)
 
 # Register your models here.
