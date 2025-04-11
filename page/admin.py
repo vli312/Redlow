1a274264ccc9dc1e241ea6e4cbf8bdcfe9b44727
FutureTerminal <zyanw236@gmail.com>
2025-04-11 14:15:38 -0400

    added data model and review in admin view

--- page/admin.py
+++ page/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Review
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'region', 'rating', 'created_at')
+    list_filter = ('rating', 'created_at')
+    search_fields = ('user__username', 'region__region_name', 'content')
+
+admin.site.register(Review, ReviewAdmin)
 
 # Register your models here.
