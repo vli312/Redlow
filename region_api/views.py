4cd4f5567c21751b0addc007ea2c0a70da166dc3
SandyVii <sandeep.kollipara@icloud.com>
2025-04-27 17:48:23 -0400

    Zipcode level MapFilter and PlotPopulate completed

--- region_api/views.py
+++ region_api/views.py
@@ -7,6 +7,8 @@ from page.models import Region, MapLocation, Neighbourhood, ZipCode, Prices
 import json
 from sklearn.metrics.pairwise import haversine_distances
 from math import radians
+import folium
+from folium.plugins import FastMarkerCluster, MarkerCluster
 
 
 LATEST_YEAR = ['2024-03-31','2024-04-30','2024-05-31','2024-06-30','2024-07-31','2024-08-31','2024-09-30','2024-10-31','2024-11-30','2024-12-31','2025-01-31','2025-02-28']
@@ -27,8 +29,40 @@ def list_neighbourhoods(request):
 
 @api_view(['PATCH'])
 def mapFilter_zipcode(request):
-    record = ZipCode.objects.get(zip_code=22305)
-    return Response(ZipCodeSerializer(record).data)
+    data = json.loads(request.body) # Testing with {"zipcode":"22305"}
+    zipcode = data["zipcode"]
+    recordZipCode = ZipCode.objects.get(zip_code=str(zipcode))
+    dataZipCode = ZipCodeSerializer(recordZipCode).data
+    region = dataZipCode["region"] # (Not) Used in the final section below
+    location = dataZipCode["location"]
+    recordMapLocation = MapLocation.objects.get(id=location)
+    dataMapLocation = MapLocationSerializer(recordMapLocation).data
+    lat1, lon1 = dataMapLocation["latitude"], dataMapLocation["longitude"]
+    zipcodes_locations = ZipCode.objects.values_list('zip_code','location')
+    zipcodes_lat_lon = []
+    for _zipcode, _location in zipcodes_locations:
+        otherRecordMapLocation = MapLocation.objects.get(id=_location)
+        otherDataMapLocation = MapLocationSerializer(otherRecordMapLocation).data
+        lat2, lon2 = otherDataMapLocation["latitude"], otherDataMapLocation["longitude"]
+        zipcodes_lat_lon.append((_zipcode, lat2, lon2))
+    # Create a Folium map object
+    center = [lat1, lon1] # Selected ZipCode
+    dmv_map = folium.Map(location=center, zoom_start=13)  # Adjust zoom_start as needed
+    marker_cluster = MarkerCluster( # Method 3: Cluster + Popup
+        name='Zip Codes Cluster',
+        overlay=True,
+        control=False,
+        icon_create_function=None)
+    for i in range(len(zipcodes_lat_lon)):
+        location = zipcodes_lat_lon[i][1], zipcodes_lat_lon[i][2]
+        marker = folium.Marker(location=location)
+        popup = zipcodes_lat_lon[i][0]
+        folium.Popup(popup).add_to(marker)
+        marker_cluster.add_child(marker)
+    marker_cluster.add_to(dmv_map)
+    folium.LayerControl().add_to(dmv_map)
+    dmv_map.save("templates/page/page_story/folium_map_zipcode.html")
+    return Response({"status":"success"})
 
 
 @api_view(['PATCH'])
